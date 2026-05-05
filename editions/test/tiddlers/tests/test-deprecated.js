BramChen/TiddlyWiki5

commit 48eeb4603a88c5bcde55fd40f004778d388211b1

Deprecate and simplify some utility functions (#9251)
* Deprecate some utility functions

* Drop IE support

* Update two function

* Update comment

* Further simplify with arrow function

* Fix node error

* Deprecate logTable

* Deprecate class functions

* Attempt to fix error

* Deprecate two functions

* Remove deprecation for getLocationPath

* Deprecate stringifyNumber, domContains, domMatchesSelector

* Deprecate $tw.utils.each

* Revert "Deprecate $tw.utils.each"

This reverts commit 650df1d575a507194688c40e980625a61b2fac07.

* Simplify getFullScreenApis

* Replace LLMap with Map

* Revert "Replace LLMap with Map"

This reverts commit 4410ac194a48816745065ab85a8012e5345d88a1.

* Move some deprecated functions to deprecated.js

* Remove Opera & MS prefix

* Deprecate getLocationPath

* Fix code style

* Revert "Remove Opera & MS prefix"

This reverts commit e5771c00be5469b3f6a8acaf6a11c1623ed97bfb.

* Revert "Simplify getFullScreenApis"

This reverts commit 894cb479eae39b792051440c78a7c816e38205a8.

* Further simplify toggleClass

* Second attempt to simplify $tw.utils.each

* Revert "Second attempt to simplify $tw.utils.each"

This reverts commit 74cb4f766e66c7330d12b29f9fc49a3379c8712f.

* Third attempt to simplify $tw.utils.each

* Add missing comma

* Update comments

* Deprecate hopArray

Since it is easy to implement it with some method

* Update change notes

* Deprecate tagToCssSelector

Since tc-tagged-* classes are deprecated

core/modules/utils/deprecated.js
@@ -1,0 +1,58 @@
+/*\
+title: $:/core/modules/utils/deprecated.js
+type: application/javascript
+module-type: utils
+
+Deprecated util functions
+
+\*/
+
+exports.logTable = data => console.table(data);
+
+exports.repeat = (str,count) => str.repeat(count);
+
+exports.startsWith = (str,search) => str.startsWith(search);
+
+exports.endsWith = (str,search) => str.endsWith(search);
+
+exports.trim = function(str) {
+	if(typeof str === "string") {
+		return str.trim();
+	} else {
+		return str;
+	}
+};
+
+exports.hopArray = (object,array) => array.some(element => $tw.utils.hop(object,element));
+
+exports.sign = Math.sign;
+
+exports.strEndsWith = (str,ending,position) => str.endsWith(ending,position);
+
+exports.stringifyNumber = num => num.toString();
+
+exports.tagToCssSelector = function(tagName) {
+	return "tc-tagged-" + encodeURIComponent(tagName).replace(/[!"#$%&'()*+,\-./:;<=>?@[\\\]^`{\|}~,]/mg,function(c) {
+		return "\\" + c;
+	});
+};
+
+exports.domContains = (a,b) => a.compareDocumentPosition(b) & 16;
+
+exports.domMatchesSelector = (node,selector) => node.matches(selector);
+
+exports.hasClass = (el,className) => el.classList && el.classList.contains(className);
+
+exports.addClass = function(el,className) {
+	el.classList && el.classList.add(className);
+};
+
+exports.removeClass = function(el,className) {
+	el.classList && el.classList.remove(className);
+};
+
+exports.toggleClass = function(el,className,status) {
+	el.classList && el.classList.toggle(className, status);
+};
+
+exports.getLocationPath = () => window.location.origin + window.location.pathname;

boot/boot.js
@@ -46,12 +46,8 @@ $tw.utils.hop = function(object,property) {
 	return object ? Object.prototype.hasOwnProperty.call(object,property) : false;
 };
 
-/*
-Determine if a value is an array
-*/
-$tw.utils.isArray = function(value) {
-	return Object.prototype.toString.call(value) == "[object Array]";
-};
+/** @deprecated Use Array.isArray instead  */
+$tw.utils.isArray = value => Array.isArray(value);
 
 /*
 Check if an array is equal by value and by reference.
@@ -130,35 +126,22 @@ $tw.utils.pushTop = function(array,value) {
 	return array;
 };
 
-/*
-Determine if a value is a date
-*/
-$tw.utils.isDate = function(value) {
-	return Object.prototype.toString.call(value) === "[object Date]";
-};
+/** @deprecated Use instanceof Date instead */
+$tw.utils.isDate = value => value instanceof Date;
 
-/*
-Iterate through all the own properties of an object or array. Callback is invoked with (element,title,object)
-*/
+/** @deprecated Use array iterative methods instead */
 $tw.utils.each = function(object,callback) {
-	var next,f,length;
 	if(object) {
-		if(Object.prototype.toString.call(object) == "[object Array]") {
-			for(f=0, length=object.length; f<length; f++) {
-				next = callback(object[f],f,object);
-				if(next === false) {
-					break;
-				}
-			}
+		if(Array.isArray(object)) {
+			object.every((element,index,array) => {
+				const next = callback(element,index,array);
+				return next !== false;
+			});
 		} else {
-			var keys = Object.keys(object);
-			for(f=0, length=keys.length; f<length; f++) {
-				var key = keys[f];
-				next = callback(object[key],key,object);
-				if(next === false) {
-					break;
-				}
-			}
+			Object.entries(object).every(entry => {
+				const next = callback(entry[1], entry[0], object);
+				return next !== false;
+			});
 		}
 	}
 };
@@ -333,32 +316,13 @@ $tw.utils.htmlDecode = function(s) {
 	return s.toString().replace(/&lt;/mg,"<").replace(/&nbsp;/mg,"\xA0").replace(/&gt;/mg,">").replace(/&quot;/mg,"\"").replace(/&amp;/mg,"&");
 };
 
-/*
-Get the browser location.hash. We don't use location.hash because of the way that Firefox auto-urldecodes it (see http://stackoverflow.com/questions/1703552/encoding-of-window-location-hash)
-*/
-$tw.utils.getLocationHash = function() {
-	var href = window.location.href;
-	var idx = href.indexOf('#');
-	if(idx === -1) {
-		return "#";
-	} else if(href.substr(idx + 1,1) === "#" ||  href.substr(idx + 1,3) === "%23") {
-		// Special case: ignore location hash if it itself starts with a #
-		return "#";
-	} else {
-		return href.substring(idx);
-	}
-};
+/** @deprecated Use window.location.hash instead.  */
+$tw.utils.getLocationHash = () => window.location.hash;
 
-/*
-Pad a string to a given length with "0"s. Length defaults to 2
-*/
-$tw.utils.pad = function(value,length) {
-	length = length || 2;
-	var s = value.toString();
-	if(s.length < length) {
-		s = "000000000000000000000000000".substr(0,length - s.length) + s;
-	}
-	return s;
+/** @deprecated Pad a string to a given length with "0"s. Length defaults to 2 */
+$tw.utils.pad = function(value,length = 2) {
+	const s = value.toString();
+	return s.padStart(length, "0");
 };
 
 // Convert a date into UTC YYYYMMDDHHMMSSmmm format

core/modules/utils/dom/dom.js
@@ -11,19 +11,6 @@ Various static DOM-related utility functions.
 
 var Popup = require("$:/core/modules/utils/dom/popup.js");
 
-/*
-Determines whether element 'a' contains element 'b'
-Code thanks to John Resig, http://ejohn.org/blog/comparing-document-position/
-*/
-exports.domContains = function(a,b) {
-	return a.contains ?
-		a !== b && a.contains(b) :
-		!!(a.compareDocumentPosition(b) & 16);
-};
-
-exports.domMatchesSelector = function(node,selector) {
-	return node.matches ? node.matches(selector) : node.msMatchesSelector(selector);
-};
 
 /*
 Select text in a an input or textarea (setSelectionRange crashes on certain input types)
@@ -46,38 +33,6 @@ exports.setSelectionByPosition = function(node,selectFromStart,selectFromEnd) {
 exports.removeChildren = function(node) {
 	while(node.hasChildNodes()) {
 		node.removeChild(node.firstChild);
-	}
-};
-
-exports.hasClass = function(el,className) {
-	return el && el.hasAttribute && el.hasAttribute("class") && el.getAttribute("class").split(" ").indexOf(className) !== -1;
-};
-
-exports.addClass = function(el,className) {
-	var c = (el.getAttribute("class") || "").split(" ");
-	if(c.indexOf(className) === -1) {
-		c.push(className);
-		el.setAttribute("class",c.join(" "));
-	}
-};
-
-exports.removeClass = function(el,className) {
-	var c = (el.getAttribute("class") || "").split(" "),
-		p = c.indexOf(className);
-	if(p !== -1) {
-		c.splice(p,1);
-		el.setAttribute("class",c.join(" "));
-	}
-};
-
-exports.toggleClass = function(el,className,status) {
-	if(status === undefined) {
-		status = !exports.hasClass(el,className);
-	}
-	if(status) {
-		exports.addClass(el,className);
-	} else {
-		exports.removeClass(el,className);
 	}
 };
 
@@ -295,10 +250,6 @@ exports.copyToClipboard = function(text,options) {
 		$tw.notifier.display(succeeded ? successNotification : failureNotification);
 	}
 	document.body.removeChild(textArea);
-};
-
-exports.getLocationPath = function() {
-	return window.location.toString().split("#")[0];
 };
 
 /*

core/modules/utils/utils.js
@@ -49,31 +49,6 @@ exports.warning = function(text) {
 };
 
 /*
-Log a table of name: value or name: [values...] pairs
-*/
-exports.logTable = function(data) {
-	var hasArrays = false;
-	$tw.utils.each(data,function(value,name) {
-		if($tw.utils.isArray(value)) {
-			hasArrays = true;
-		}
-	});
-	if(console.table && !hasArrays) {
-		console.table(data);
-	} else {
-		$tw.utils.each(data,function(value,name) {
-			if($tw.utils.isArray(value)) {
-				for(var t=0; t<value.length; t++) {
-					console.log(`${name}[${t}]: ${value[t]}`);
-				}
-			} else {
-				console.log(`${name}: ${value}`);
-			}
-		});
-	}
-}
-
-/*
 Return the integer represented by the str (string).
 Return the dflt (default) parameter if str is not a base-10 number.
 */
@@ -89,43 +64,6 @@ exports.replaceString = function(text,search,replace) {
 	return text.replace(search,function() {
 		return replace;
 	});
-};
-
-/*
-Repeats a string
-*/
-exports.repeat = function(str,count) {
-	var result = "";
-	for(var t=0;t<count;t++) {
-		result += str;
-	}
-	return result;
-};
-
-/*
-Check if a string starts with another string
-*/
-exports.startsWith = function(str,search) {
-	return str.substring(0, search.length) === search;
-};
-
-/*
-Check if a string ends with another string
-*/
-exports.endsWith = function(str,search) {
-	return str.substring(str.length - search.length) === search;
-};
-
-/*
-Trim whitespace from the start and end of a string
-Thanks to Steven Levithan, http://blog.stevenlevithan.com/archives/faster-trim-javascript
-*/
-exports.trim = function(str) {
-	if(typeof str === "string") {
-		return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-	} else {
-		return str;
-	}
 };
 
 exports.trimPrefix = function(str,unwanted) {
@@ -210,18 +148,6 @@ Return the number of keys in an object
 */
 exports.count = function(object) {
 	return Object.keys(object || {}).length;
-};
-
-/*
-Determine whether an array-item is an object-property
-*/
-exports.hopArray = function(object,array) {
-	for(var i=0; i<array.length; i++) {
-		if($tw.utils.hop(object,array[i])) {
-			return true;
-		}
-	}
-	return false;
 };
 
 /*
@@ -953,44 +879,6 @@ exports.makeDataUri = function(text,type,_canonical_uri) {
 };
 
 /*
-Useful for finding out the fully escaped CSS selector equivalent to a given tag. For example:
-
-$tw.utils.tagToCssSelector("$:/tags/Stylesheet") --> tc-tagged-\%24\%3A\%2Ftags\%2FStylesheet
-*/
-exports.tagToCssSelector = function(tagName) {
-	return "tc-tagged-" + encodeURIComponent(tagName).replace(/[!"#$%&'()*+,\-./:;<=>?@[\\\]^`{\|}~,]/mg,function(c) {
-		return "\\" + c;
-	});
-};
-
-/*
-IE does not have sign function
-*/
-exports.sign = Math.sign || function(x) {
-	x = +x; // convert to a number
-	if(x === 0 || isNaN(x)) {
-		return x;
-	}
-	return x > 0 ? 1 : -1;
-};
-
-/*
-IE does not have an endsWith function
-*/
-exports.strEndsWith = function(str,ending,position) {
-	if(str.endsWith) {
-		return str.endsWith(ending,position);
-	} else {
-		if(typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > str.length) {
-			position = str.length;
-		}
-		position -= ending.length;
-		var lastIndex = str.indexOf(ending, position);
-		return lastIndex !== -1 && lastIndex === position;
-	}
-};
-
-/*
 Return system information useful for debugging
 */
 exports.getSystemInfo = function(str,ending,position) {
@@ -1014,10 +902,6 @@ exports.parseNumber = function(str) {
 
 exports.parseInt = function(str) {
 	return parseInt(str,10) || 0;
-};
-
-exports.stringifyNumber = function(num) {
-	return num + "";
 };
 
 exports.makeCompareFunction = function(type,options) {

editions/tw5.com/tiddlers/releasenotes/5.4.0/deprecate-util-func - deprecation.tid
@@ -1,0 +1,8 @@
+title: $:/changenotes/5.4.0/#9251/impacts/deprecation
+changenote: $:/changenotes/5.4.0/#9251
+created: 20251129130610944
+modified: 20251129130610953
+tags: $:/tags/ImpactNote
+description: Some utility functions are deprecated and are discouraged to be used
+impact-type: deprecation
+

editions/tw5.com/tiddlers/releasenotes/5.4.0/deprecate-util-func.tid
@@ -1,0 +1,10 @@
+title: $:/changenotes/5.4.0/#9251
+description: Deprecate some utility functions
+release: 5.4.0
+tags: $:/tags/ChangeNote
+change-type: deprecation
+change-category: developer
+github-links: https://github.com/TiddlyWiki/TiddlyWiki5/pull/9251
+github-contributors: Leilei332
+
+Deprecate some utility functions. Some of them are moved to [[$:/core/modules/utils/deprecated.js]].

commit 56ea0789e184f92f82bda49df20e4ee9c43b0a5a

Update deprecated.js issues (#9833)
* Fix RSOD when $tw.utils.addClass receives a class string with whitespace

PR #9251 replaced the manual setAttribute("class", ...) implementation of
$tw.utils.addClass/removeClass/toggleClass with direct Element.classList
calls. Unlike setAttribute, classList.add/remove/toggle throws
InvalidCharacterError on any token containing whitespace, so callers that
pass a whole class string (e.g. modal.js passing tiddler.fields.class)
now crash.

Manual repro on tw5-com: open SampleWizard, set the `class` field to
"aaa bbb", Done, open popup -> OK -> open nested popup -> RSOD.

Fix: split the className argument on whitespace in deprecated.js and feed
individual tokens to classList. A small splitClasses() helper keeps the
three functions symmetrical.

Adds adversarial regression tests in test-utils.js covering:
- ASCII whitespace variants (space, tab, CR, LF, mixed runs, padding)
- Unicode whitespace (U+00A0 non-breaking space)
- de-duplication across single and multiple calls
- remove/toggle no-op on missing tokens
- toggle with status undefined / true / false
- silent no-op for whitespace-only / empty / non-string / null input
- silent no-op when the element has no classList

* Move new tests to their own file

* Add backwards-compat regression tests for deprecated.js

Locks in pre-5.4.0 tolerant behaviour of $:/core/modules/utils/
deprecated.js helpers that regressed in PR #9251. Each spec targets an
edge-case input the current one-line modern equivalents reject:

- repeat: negative count / null / undefined str
- startsWith / endsWith: RegExp search arg
- stringifyNumber: null / undefined
- domContains: boolean return, self-check
- hasClass: null element, classless element
- getLocationPath: query preservation, hash stripping
  (browser-only; pends in Node because the TW5 sandbox has no `window`)

Also picks up the addClass/removeClass/toggleClass whitespace specs
moved out of test-utils.js by the previous commit, so all deprecated.js

core/modules/utils/deprecated.js
@@ -3,18 +3,43 @@ title: $:/core/modules/utils/deprecated.js
 type: application/javascript
 module-type: utils
 
-Deprecated util functions
+Deprecated util functions. These preserve the pre-5.4.0 signatures and
+behaviour for backwards compatibility with plugins and external scripts.
+Prefer modern alternatives in new code (Array.prototype methods, classList,
+Math.sign, String.prototype.repeat, etc.).
 
 \*/
 
 exports.logTable = (data) => console.table(data);
 
-exports.repeat = (str,count) => str.repeat(count);
+/*
+Repeats a string
+*/
+exports.repeat = function(str,count) {
+	var result = "";
+	for(var t=0;t<count;t++) {
+		result += str;
+	}
+	return result;
+};
 
-exports.startsWith = (str,search) => str.startsWith(search);
+/*
+Check if a string starts with another string
+*/
+exports.startsWith = function(str,search) {
+	return str.substring(0, search.length) === search;
+};
 
-exports.endsWith = (str,search) => str.endsWith(search);
+/*
+Check if a string ends with another string
+*/
+exports.endsWith = function(str,search) {
+	return str.substring(str.length - search.length) === search;
+};
 
+/*
+Trim whitespace from the start and end of a string
+*/
 exports.trim = function(str) {
 	if(typeof str === "string") {
 		return str.trim();
@@ -29,30 +54,54 @@ exports.sign = Math.sign;
 
 exports.strEndsWith = (str,ending,position) => str.endsWith(ending,position);
 
-exports.stringifyNumber = (num) => num.toString();
+exports.stringifyNumber = function(num) {
+	return num + "";
+};
 
+// Returns the fully escaped CSS selector for a tag, e.g.
+// "$:/tags/Stylesheet" -> "tc-tagged-\%24\%3A\%2Ftags\%2FStylesheet" 
 exports.tagToCssSelector = function(tagName) {
 	return "tc-tagged-" + encodeURIComponent(tagName).replace(/[!"#$%&'()*+,\-./:;<=>?@[\\\]^`{\|}~,]/mg,function(c) {
 		return "\\" + c;
 	});
 };
 
-exports.domContains = (a,b) => a.compareDocumentPosition(b) & 16;
+/*
+Determines whether element 'a' contains element 'b'.
+Returns false when a === b (matches the original John Resig semantics).
+*/
+exports.domContains = function(a,b) {
+	return a !== b && a.contains(b);
+};
 
 exports.domMatchesSelector = (node,selector) => node.matches(selector);
 
-exports.hasClass = (el,className) => el.classList && el.classList.contains(className);
+exports.hasClass = function(el,className) {
+	return !!(el && el.classList && el.classList.contains(className));
+};
+
+// addClass/removeClass/toggleClass split on whitespace to preserve the
+// original setAttribute("class", ...) acceptance of "foo bar" as two
+// classes. Regressed in #9251.
+function splitClasses(className) {
+	return (typeof className === "string" && className.match(/\S+/g)) || [];
+}
 
 exports.addClass = function(el,className) {
-	el.classList && className && el.classList.add(className);
+	if(!el.classList) return;
+	splitClasses(className).forEach(function(c) { el.classList.add(c); });
 };
 
 exports.removeClass = function(el,className) {
-	el.classList && className && el.classList.remove(className);
+	if(!el.classList) return;
+	splitClasses(className).forEach(function(c) { el.classList.remove(c); });
 };
 
 exports.toggleClass = function(el,className,status) {
-	el.classList && className && el.classList.toggle(className, status);
+	if(!el.classList) return;
+	splitClasses(className).forEach(function(c) { el.classList.toggle(c,status); });
 };
 
-exports.getLocationPath = () => window.location.origin + window.location.pathname;
+exports.getLocationPath = function() {
+	return window.location.toString().split("#")[0];
+};

editions/test/tiddlers/tests/test-deprecated.js
@@ -1,0 +1,252 @@
+/*\
+title: test-deprecated.js
+type: application/javascript
+tags: [[$:/tags/test-spec]]
+
+Regression-guard tests for $:/core/modules/utils/deprecated.js.
+
+Locks in pre-5.4.0 tolerant behaviour of $tw.utils helpers that regressed
+in PR #9251 (one-line modern equivalents diverge on edge-case inputs).
+Without the companion restoration patch to core/modules/utils/deprecated.js:
+8 specs fail. With the patch: green.
+
+The addClass/removeClass/toggleClass specs at the end were moved from
+test-utils.js — RSOD guard for the SampleWizard report (class field
+"aaa bbb" crashing classList.add with InvalidCharacterError).
+
+\*/
+
+"use strict";
+
+describe("deprecated.js — backwards-compat",function() {
+
+	describe("$tw.utils.repeat",function() {
+		it("returns '' for zero or negative count (does not throw)",function() {
+			expect($tw.utils.repeat("x",0)).toBe("");
+			expect($tw.utils.repeat("x",-1)).toBe("");
+			expect($tw.utils.repeat("x",-100)).toBe("");
+		});
+		it("coerces null/undefined str rather than throwing",function() {
+			expect($tw.utils.repeat(null,3)).toBe("nullnullnull");
+			expect($tw.utils.repeat(undefined,2)).toBe("undefinedundefined");
+		});
+		it("still works for normal inputs",function() {
+			expect($tw.utils.repeat("ab",3)).toBe("ababab");
+			expect($tw.utils.repeat("-",5)).toBe("-----");
+		});
+	});
+
+	describe("$tw.utils.startsWith / $tw.utils.endsWith",function() {
+		it("tolerate a RegExp search argument without throwing",function() {
+			// Old impl uses substring coercion; native String.prototype.startsWith
+			// throws TypeError when passed a RegExp.
+			expect(function() { $tw.utils.startsWith("abc",/a/); }).not.toThrow();
+			expect(function() { $tw.utils.endsWith("abc",/c/); }).not.toThrow();
+		});
+		it("still match normal string inputs",function() {
+			expect($tw.utils.startsWith("abcdef","abc")).toBe(true);
+			expect($tw.utils.startsWith("abcdef","xyz")).toBe(false);
+			expect($tw.utils.endsWith("abcdef","def")).toBe(true);
+			expect($tw.utils.endsWith("abcdef","xyz")).toBe(false);
+		});
+	});
+
+	describe("$tw.utils.stringifyNumber",function() {
+		it("coerces null/undefined via string-concat rather than throwing",function() {
+			expect($tw.utils.stringifyNumber(null)).toBe("null");
+			expect($tw.utils.stringifyNumber(undefined)).toBe("undefined");
+		});
+		it("still returns a number's string form",function() {
+			expect($tw.utils.stringifyNumber(42)).toBe("42");
+			expect($tw.utils.stringifyNumber(-3.14)).toBe("-3.14");
+			expect($tw.utils.stringifyNumber(0)).toBe("0");
+		});
+	});
+
+	describe("$tw.utils.domContains",function() {
+		// Stub nodes expose both .contains() and .compareDocumentPosition() so
+		// both the old (compareDocumentPosition & 16 → number) and new
+		// (a !== b && a.contains(b) → boolean) forms can be observed.
+		function makeNode(children) {
+			children = children || [];
+			var self;
+			self = {
+				contains: function(other) {
+					if(other === self) { return true; }
+					return children.some(function(c) { return c === other || c.contains(other); });
+				},
+				compareDocumentPosition: function(other) {
+					if(other === self) { return 0; }
+					return self.contains(other) ? 16 : 0;
+				}
+			};
+			return self;
+		}
+		it("returns strictly boolean true/false, not a bit-mask number",function() {
+			var child = makeNode();
+			var parent = makeNode([child]);
+			var unrelated = makeNode();
+			expect($tw.utils.domContains(parent,child)).toBe(true);
+			expect($tw.utils.domContains(parent,unrelated)).toBe(false);
+		});
+		it("returns false for domContains(x, x)",function() {
+			var a = makeNode();
+			expect($tw.utils.domContains(a,a)).toBe(false);
+		});
+	});
+
+	describe("$tw.utils.hasClass",function() {
+		it("returns false for null/undefined element without throwing",function() {
+			expect(function() { $tw.utils.hasClass(null,"foo"); }).not.toThrow();
+			expect($tw.utils.hasClass(null,"foo")).toBe(false);
+			expect(function() { $tw.utils.hasClass(undefined,"foo"); }).not.toThrow();
+			expect($tw.utils.hasClass(undefined,"foo")).toBe(false);
+		});
+		it("returns strictly false (not undefined) for elements without classList",function() {
+			expect($tw.utils.hasClass({},"foo")).toBe(false);
+		});
+		it("delegates to classList.contains for real elements",function() {
+			var el = { classList: { contains: function(c) { return c === "a" || c === "b"; } } };
+			expect($tw.utils.hasClass(el,"a")).toBe(true);
+			expect($tw.utils.hasClass(el,"b")).toBe(true);
+			expect($tw.utils.hasClass(el,"c")).toBe(false);
+		});
+	});
+
+	// getLocationPath reads window.location: specs pend in Node (no `window`
+	// in the TW5 sandbox) and use history.replaceState in the browser —
+	// assigning to window.location would trigger a navigation and reload.
+	describe("$tw.utils.getLocationPath",function() {
+		var originalUrl;
+		beforeEach(function() {
+			if(!$tw.browser) { return; }
+			originalUrl = window.location.href;
+		});
+		afterEach(function() {
+			if(!$tw.browser) { return; }
+			history.replaceState(null,"",originalUrl);
+		});
+		it("preserves the query string in the returned path",function() {
+			if(!$tw.browser) { pending("browser-only: requires window.location - run in browser"); return; }
+			history.replaceState(null,"","?lang=de&x=1#Intro");
+			var path = $tw.utils.getLocationPath();
+			expect(path).toContain("?lang=de&x=1");
+			expect(path).not.toContain("#Intro");
+		});
+		it("strips the hash fragment",function() {
+			if(!$tw.browser) { pending("browser-only: requires window.location - run in browser"); return; }
+			history.replaceState(null,"","#SomeTiddler");
+			// Sanity check: replaceState actually changed the hash.
+			expect(window.location.hash).toBe("#SomeTiddler");
+			var path = $tw.utils.getLocationPath();
+			expect(path).not.toContain("#");
+			// Rebuild expected href without the hash — works on http(s):// and file://.
+			var expected = window.location.href.split("#")[0];
+			expect(path).toBe(expected);
+		});
+		it("includes the query string when no hash is present",function() {
+			if(!$tw.browser) { pending("browser-only: requires window.location - run in browser"); return; }
+			history.replaceState(null,"","?x=1");
+			var path = $tw.utils.getLocationPath();
+			expect(path).toMatch(/\?x=1$/);
+			expect(path).not.toContain("#");
+		});
+	});
+
+	// Regression guard: classList.add/remove/toggle throw InvalidCharacterError on
+	// whitespace. Manual repro: tw5-com #SampleWizard, class="aaa bbb", Done, popup
+	// -> OK -> nested popup -> RSOD. Stub classList mirrors real DOM semantics
+	// (reject whitespace, de-dupe on add, no-op on remove of missing token).
+	describe("addClass/removeClass/toggleClass",function() {
+		function makeEl() {
+			var tokens = [];
+			function reject(t) { if(/\s/.test(t)) { throw new Error("InvalidCharacterError: '" + t + "'"); } }
+			return {
+				classList: {
+					add: function() {
+						for(var i = 0; i < arguments.length; i++) {
+							reject(arguments[i]);
+							if(tokens.indexOf(arguments[i]) === -1) { tokens.push(arguments[i]); }
+						}
+					},
+					remove: function() {
+						for(var i = 0; i < arguments.length; i++) {
+							reject(arguments[i]);
+							var idx = tokens.indexOf(arguments[i]);
+							if(idx !== -1) { tokens.splice(idx,1); }
+						}
+					},
+					toggle: function(cls,status) {
+						reject(cls);
+						var has = tokens.indexOf(cls) !== -1;
+						var want = status === undefined ? !has : status;
+						if(want && !has) { tokens.push(cls); }
+						if(!want && has) { tokens.splice(tokens.indexOf(cls),1); }
+					}
+				},
+				_tokens: tokens
+			};
+		}
+
+		it("splits on every ASCII-whitespace flavour (space, tab, newline, CR, mixed runs, leading/trailing)",function() {
+			var el = makeEl();
+			$tw.utils.addClass(el,"  a\tb\nc\r\nd  \t  e  ");
+			expect(el._tokens).toEqual(["a","b","c","d","e"]);
+		});
+
+		it("splits on Unicode whitespace too (U+00A0 non-breaking space, a common paste-in hazard)",function() {
+			var el = makeEl();
+			$tw.utils.addClass(el,"a\u00A0b");
+			expect(el._tokens).toEqual(["a","b"]);
+		});
+
+		it("de-duplicates tokens within one call and across calls",function() {
+			var el = makeEl();
+			$tw.utils.addClass(el,"x x y");
+			$tw.utils.addClass(el,"y z");
+			expect(el._tokens).toEqual(["x","y","z"]);
+		});
+
+		it("remove is a no-op for missing tokens and tolerates mixed-presence input",function() {
+			var el = makeEl();
+			$tw.utils.addClass(el,"a b");
+			$tw.utils.removeClass(el,"b c d");
+			expect(el._tokens).toEqual(["a"]);
+		});
+
+		it("toggle with no status flips each token independently",function() {
+			var el = makeEl();
+			$tw.utils.addClass(el,"a");
+			$tw.utils.toggleClass(el,"a b");
+			expect(el._tokens).toEqual(["b"]);
+		});
+
+		it("toggle with status=true/false forces state regardless of current",function() {
+			var el = makeEl();
+			$tw.utils.addClass(el,"a");
+			$tw.utils.toggleClass(el,"a b",true);
+			expect(el._tokens).toEqual(["a","b"]);
+			$tw.utils.toggleClass(el,"a b",false);
+			expect(el._tokens).toEqual([]);
+		});
+
+		it("is a silent no-op for whitespace-only / empty / non-string / null / undefined className",function() {
+			var el = makeEl();
+			var inputs = ["", "   \t\n ", null, undefined, 42, {}, ["a"]];
+			inputs.forEach(function(v) {
+				expect(function() { $tw.utils.addClass(el,v); }).not.toThrow();
+				expect(function() { $tw.utils.removeClass(el,v); }).not.toThrow();
+				expect(function() { $tw.utils.toggleClass(el,v); }).not.toThrow();
+			});
+			expect(el._tokens).toEqual([]);
+		});
+
+		it("is a silent no-op when element has no classList (SVG in old browsers, detached nodes, stubs)",function() {
+			var el = {};
+			expect(function() { $tw.utils.addClass(el,"a b"); }).not.toThrow();
+			expect(function() { $tw.utils.removeClass(el,"a b"); }).not.toThrow();
+			expect(function() { $tw.utils.toggleClass(el,"a b",true); }).not.toThrow();
+		});
+	});
+
+});